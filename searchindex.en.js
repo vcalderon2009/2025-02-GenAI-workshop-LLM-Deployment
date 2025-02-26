f4748bf6be5cc05765dfe381d1c82d87302f771b
Victor Calderon <2266703+vcalderon2009@users.noreply.github.com>
2025-02-25 22:29:22 -0800

    Publishing to gh-pages (publish.sh)

---
+++ searchindex.en.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "LLM Deployment - Workshop",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "LLM Deployment - Workshop",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "LLM Deployment - Workshop",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
